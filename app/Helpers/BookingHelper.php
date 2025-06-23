ff9c6acc039331c4a1857f8b000c11eebb1673c2
vishnuavanexa <vishnuavanexa@gmail.com>
2025-06-23 19:40:54 +0530

    convert the database time values to 24-hour format

--- app/Helpers/BookingHelper.php
+++ app/Helpers/BookingHelper.php
@@ -11,8 +11,8 @@ class BookingHelper
         return Booking::where('venue_id', $venue_id)
             ->where('booking_date', $booking_date)
             ->where(function ($q) use ($start_time, $end_time) {
-                $q->where('start_time', '<', $end_time)
-                    ->where('end_time', '>', $start_time);
+                $q->whereRaw("STR_TO_DATE(start_time, '%h:%i %p') < STR_TO_DATE(?, '%H:%i')", [$end_time])
+                    ->whereRaw("STR_TO_DATE(end_time, '%h:%i %p') > STR_TO_DATE(?, '%H:%i')", [$start_time]);
             })
             ->exists();
     }
